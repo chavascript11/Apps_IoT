82d7118663c41bceaa6fd4ba4945473c00c520e5
Salvador Orozco Gutiérrez <salvadororozcogtz11@gmail.com>
2023-09-05 14:25:29 -0600

    quantity added

--- Unidad 1/server.py
+++ Unidad 1/server.py
@@ -23,9 +23,9 @@ class MyHTTPRequestHandler(BaseHTTPRequestHandler):
         global contador
 
         if(body_json['action'] == 'asc'):
-            contador += 1
-        elif(body_json[action]== 'desc'):
-            contador -= 1
+            contador += body_json['quantity']
+        elif(body_json['action']== 'desc'):
+            contador -= body_json['quantity']
 
         # Print the complete HTTP request
         print("\n----- Incoming POST Request -----")
